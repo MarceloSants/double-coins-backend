0ee6b74ff727a44c0e7142bdc07ffafbb2826266
Marcelo Santos <marcelosantos220716@gmail.com>
2024-10-17 19:42:05 -0300

    Add basic configs
    - Express
    - Sequelize

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('server running!');
+const express = require('express');
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+const usersRoute = require('./src/routes/users');
+const earnsRoute = require('./src/routes/earns');
+const expensesRoute = require('./src/routes/expenses');
+
+var cors = require('cors');
+app.use(cors());
+
+app.use('/users', usersRoute);
+app.use('/earns', earnsRoute);
+app.use('/expenses', expensesRoute);
+
+app.listen(port, () => {
+  console.log(`Node.js HTTP server is running on port ${port}`);
+});
